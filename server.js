d9f97239e496af12b70b21a7846482909c61a508
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-07-22 19:30:07 +0200

    Dodani su modeli (ad, book, chat, seller) u backendu. Dodane su routes (adRoutes, auth, bookRoutes, chatRoutes, sellerRoutes).

--- server.js
+++ server.js
@@ -5,25 +5,33 @@ const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 
 const authRoutes = require("./routes/auth");
+const bookRoutes = require("./routes/bookRoutes");
+const sellerRoutes = require("./routes/sellerRoutes");
+const adRoutes = require("./routes/adRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 
 const app = express();
 
 app.use(cors({ 
-    origin: 'http://localhost:8081/',
+    origin: 'http://localhost:8081',
     credentials: true
 }));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use("/auth",authRoutes);
 
-mongoose.connect(process.env.MONGODB_URI)
+app.use("/auth", authRoutes);
+app.use("/books", bookRoutes);
+app.use("/sellers", sellerRoutes);
+app.use("/ads", adRoutes);
+app.use("/chats", chatRoutes);
 
-.then(() => console.log('Connected to Database'))
-.catch(err => console.error('Could not connect to Database'));
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to Database'))
+    .catch(err => console.error('Could not connect to Database'));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
