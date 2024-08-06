7763d4383ff5f3d80dd9da34f57f7535e845222f
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-08-06 22:06:50 +0200

    promijene 1

--- server.js
+++ server.js
@@ -27,5 +27,6 @@ app.use('/sellers', sellerRoutes);
 app.use('/users', userRoutes);
 app.use('/auth', authRoutes); 
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
