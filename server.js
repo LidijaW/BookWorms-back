54a0e4d2f5a972631b150d4e5738f4392c1239d6
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-08-09 21:27:28 +0200

    mongodb promjena

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ import authMiddleware from './middleware/authMiddleware.js';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
