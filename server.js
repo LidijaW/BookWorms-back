20fd73df6c4f06b8763931f5aeaf44600416cb24
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-08-31 14:58:48 +0200

    server.js autocorrect

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import cors from "cors"; 
+import cors from "cors";
 import bookRoutes from "./routes/bookRoutes.js";
 import authRoutes from "./routes/auth.js";
 import adRoutes from "./routes/adRoutes.js";
 import sellerRoutes from "./routes/sellerRoutes.js";
-
 
 dotenv.config();
 
@@ -19,25 +18,17 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const corsOptions = {
-  origin: [
-    "http://localhost:8081",
-
-  ],
+  origin: "http://localhost:8081", 
   methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   credentials: true,
 };
 
-app.use(cors(corsOptions)); 
-
-app.use(cors());
-
-app.use(bodyParser.json());
-
+app.use(cors(corsOptions));
 
 app.use("/bookworms/auth", authRoutes);
-app.use("/bookworms/books",bookRoutes);
-app.use("/bookworms/ads",adRoutes);
-app.use("/bookworms/sellers",sellerRoutes);
+app.use("/bookworms/books", bookRoutes);
+app.use("/bookworms/ads", adRoutes);
+app.use("/bookworms/sellers", sellerRoutes);
 
 mongoose
   .connect(process.env.MONGODB_URI, {
@@ -47,7 +38,7 @@ mongoose
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
+      console.log(`Server running on port ${PORT}`); 
     });
   })
   .catch((error) => {
