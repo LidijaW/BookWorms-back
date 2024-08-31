befd4185ffa3b8a754e31ccbf09a19cf33d4e131
Lidl <58550278+LidijaW@users.noreply.github.com>
2024-08-31 13:03:35 +0200

    spajanje backend s frontendom

--- server.js
+++ server.js
@@ -1,35 +1,55 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
-import bookRoutes from './routes/bookRoutes.js';
-import authRoutes from './routes/auth.js';
-import adRoutes from './routes/adRoutes.js';
-import sellerRoutes from './routes/sellerRoutes.js';
-import authMiddleware from './middleware/authMiddleware.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+import cors from "cors"; 
+import bookRoutes from "./routes/bookRoutes.js";
+import authRoutes from "./routes/auth.js";
+import adRoutes from "./routes/adRoutes.js";
+import sellerRoutes from "./routes/sellerRoutes.js";
+
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(cookieParser());
 app.use(bodyParser.json());
 
-app.use('/auth', authRoutes);
+const corsOptions = {
+  origin: [
+    "http://localhost:8081",
 
-// Protected routes
-app.use('/books', bookRoutes);
-app.use('/ads', authMiddleware, adRoutes);
-app.use('/sellers', authMiddleware, sellerRoutes);
+  ],
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+};
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+app.use(cors(corsOptions)); 
+
+app.use(cors());
+
+app.use(bodyParser.json());
+
+
+app.use("/bookworms/auth", authRoutes);
+app.use("/bookworms/books",bookRoutes);
+app.use("/bookworms/ads",adRoutes);
+app.use("/bookworms/sellers",sellerRoutes);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB", error);
   });
-}).catch((error) => {
-  console.error('Error connecting to MongoDB', error);
-});
